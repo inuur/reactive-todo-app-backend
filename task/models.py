41e5ec0878a3969a035b114aa12ec55e67510a27
Ainur <inuur@mail.ru>
2020-06-20 16:57:20 +0300

    git repo location changed. django migrations

---
+++ task/models.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.db import models
+
+
+class List(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    background_image = models.CharField(max_length=100)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=500)
+    user_list = models.ForeignKey(List, on_delete=models.CASCADE)
+    deadline = models.DateField()
+    order_num = models.IntegerField()
